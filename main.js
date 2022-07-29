1345c150f6de748c59d4fd6b0fcc653a41ed49ee
Abbe <31761483+Abbe-Engers@users.noreply.github.com>
2022-07-29 23:57:59 +0200

    test

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545" );
 // https://info.uniswap.org/pair/0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc
 const uniswapUsdtWethExchange = "0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc";
 
+
 // this ABI object works for both Uniswap and SushiSwap
 const uniswapAbi = [
   "event Swap(address indexed sender, uint amount0In, uint amount1In, uint amount0Out, uint amount1Out, address indexed to)",
